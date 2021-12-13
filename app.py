1b58efa3f25cde88d358203ec77bc96ecd9bb131
Matthew Novelli <novelli3@illinois.edu>
2021-12-12 21:45:09 -0600

    implementing twitter

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ import os, random
 import requests
 from moviepy.audio import *
 from moviepy.editor import *
-# from moviepy.audio.io.AudioFileClip import AudioFileClip
-# from keys import key, secret, bearer
+from keys import key, secret, bearer
 from internetarchive import *
 import urllib.parse
 import json
 import time
+from tinytag import TinyTag
+import tweepy
+
+
+auth = tweepy.OAuthHandler(consumer_key=key, consumer_secret=secret)
+birdapp = tweepy.API(auth)
+
+
+
 
 finallength = random.randrange(5, 15)
+
 print("this video will be", finallength, "seconds long")
 
 search = search_items("collection=silent_films", fields=["format","title","identifier"])
@@ -20,10 +29,17 @@ while pick == "":
     if "MPEG4" not in pick["format"]:
         pick = ""
 
+video_attribution = ""
+
 print("media identifier:", pick["identifier"])
 metadata = requests.get(f'https://archive.org/metadata/{pick["identifier"]}').json()
 for file in metadata["files"]:
     if "MPEG4" in file["format"]:
+        if "year" in metadata["metadata"]:
+            video_attribution = "🎞️: " + metadata["metadata"]["title"][:100] + " (" + metadata["metadata"]["year"] + ")"
+        else:
+            video_attribution = "🎞️: " + metadata["metadata"]["title"][:100]
+        print(video_attribution)
         filehost = urllib.parse.quote(f'{metadata["d1"]}{metadata["dir"]}/{file["name"]}')
         length = file["length"]
         timestart = int(random.randrange(0, int(float(length)) - finallength))
@@ -39,13 +55,30 @@ video = VideoFileClip("raw.mp4", audio=False)
 filename = random.choice(os.listdir("music/"))
 print(f"music: music/{filename}")
 
+audio_data = TinyTag.get(f"music/{filename}")
 audio = AudioFileClip(f"music/{filename}")
+
+audio_attribution = "🎵: "
+if audio_data.title is not None and audio_data.artist is not None:
+    audio_attribution = audio_attribution + audio_data.title + " — " + audio_data.artist
+else:
+    audio_attribution = audio_attribution + filename
+
+print(audio_attribution)
+
 randompoint_audio = random.randrange(0, int(audio.duration) - finallength)
 audio = audio.subclip(randompoint_audio, randompoint_audio + finallength)
 
 new_audioclip = CompositeAudioClip([audio])
 video.audio = new_audioclip
 video.write_videofile("export.mp4")
+
+time.sleep(10)
+print("waiting again for file...")
+uploadedmedia = birdapp.media_upload(filename="export.mp4")
+statustext = audio_attribution + "\n" + video_attribution
+videotweet = birdapp.update_status(media_ids=uploadedmedia["id"])
+replytweet = birdapp.update_status(status=statustext, in_reply_to_status_id=videotweet["id"])
 
 #######################################################################
 # choose random audio file from directory ✓
